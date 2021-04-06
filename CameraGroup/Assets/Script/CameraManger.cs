dd8e017a2d4af6f027f86f1c63f466bf6cf8d92b
PeachyPear1 <372018@colum.edu>
2021-04-05 19:59:24 -0500

    Added SwitchViews
    Put in code

--- CameraGroup/Assets/Script/CameraManger.cs
+++ CameraGroup/Assets/Script/CameraManger.cs
@@ -8,9 +8,9 @@ public class CameraManger : MonoBehaviour
     // Start is called before the first frame update
 
 
-    public Camera mainCamera; 
-    public Camera pictureInPictureCam; 
-    public Camera SideViewCamera; 
+    public Camera mainCamera;
+    public Camera pictureInPictureCam;
+    public Camera SideViewCamera;
     public Camera SplitScreenUpper;
     public Camera SplitScreenLower;
 
@@ -18,17 +18,18 @@ public class CameraManger : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.mouseScrollDelta.y != 0)
-        { Vector3 diff = Vector3.forward * Input.mouseScrollDelta.y;
+        {
+            Vector3 diff = Vector3.forward * Input.mouseScrollDelta.y;
             if (mainCamera.enabled) mainCamera.transform.Translate(diff);
             else if (SideViewCamera.enabled) SideViewCamera.transform.Translate(diff);
-            else if (SplitScreenLower.enabled) SplitScreenLower.transform.Translate(diff); 
+            else if (SplitScreenLower.enabled) SplitScreenLower.transform.Translate(diff);
         }
     }
 
@@ -40,16 +41,48 @@ public class CameraManger : MonoBehaviour
         else if (SplitScreenLower.enabled) SplitScreenLower.nearClipPlane = value;
     }
 
-   
+
     public void SwitchViews(Dropdown change)
     {
         Debug.LogFormat("{0}:{1}", change.itemText.text, change.value);
-
-
-
-
-
+        switch (change.value)
+        {            //Show Camera 1 view only
+            case 0:
+                mainCamera.enabled = true;
+                pictureInPictureCam.enabled = false;
+                SideViewCamera.enabled = false;
+                SplitScreenUpper.enabled = false;
+                SplitScreenLower.enabled = false;
+                break;
+            // Show Camera 2 view only
+            case 1:
+                mainCamera.enabled = false;
+                pictureInPictureCam.enabled = false;
+                SideViewCamera.enabled = true;
+                SplitScreenUpper.enabled = false;
+                SplitScreenLower.enabled = false;
+                break;
+            // Show Picture In Picture
+            // With main camera enabled
+            case 2:
+                mainCamera.enabled = true;
+                pictureInPictureCam.enabled = true;
+                SideViewCamera.enabled = false;
+                SplitScreenUpper.enabled = false;
+                SplitScreenLower.enabled = false;
+                break;
+            // Show Split Screen
+            case 3:
+                mainCamera.enabled = false;
+                pictureInPictureCam.enabled = false;
+                SideViewCamera.enabled = false;
+                SplitScreenUpper.enabled = true;
+                SplitScreenLower.enabled = true;
+                break;
+        }
     }
-
-
 }
+
+
+        
+    
